c704ab0bec68599745edd2400673cc81051d799c
Ujjwal Singh Shekhawat <ujjus2001@gmail.com>
2020-04-28 01:14:29 +0530

    New emojis yay

--- mainscript.js
+++ mainscript.js
@@ -10,13 +10,12 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (socket) {
   socket.on('username', function (username) {
     socket.username = username;
-    io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    io.emit('is_online', '💠 <i>' + socket.username + ' join the chat..</i>');
   });
 
   socket.on('disconnect', function (username) {
-    io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    io.emit('is_online', '🔶 <i>' + socket.username + ' left the chat..</i>');
   });
-
   socket.on('chat_message', function (message) {
     io.emit(
       'chat_message',
