d5cf8e77086f371214769f08117f3454f035bb20
Ujjwal Singh Shekhawat <ujjus2001@gmail.com>
2020-04-27 22:06:21 +0530

    .

--- mainscript.js
+++ mainscript.js
@@ -1,32 +1,20 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.render('mainpage.ejs');
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function () {
+  console.log('Chat server running');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.on('username', function (username) {
-    socket.username = username;
-    io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    io.emit('message', msg);
   });
-
-  socket.on('disconnect', function (username) {
-    io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-  });
-
-  socket.on('chat_message', function (message) {
-    io.emit(
-      'chat_message',
-      '<strong>' + socket.username + '</strong>: ' + message
-    );
-  });
-});
-
-const PORT = process.env.PORT || 8080;
-
-const server = http.listen(PORT, function () {
-  console.log('listening on port ' + PORT);
 });
